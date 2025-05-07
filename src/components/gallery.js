b9bdf65b44502655e515e08f3226cd234eef3b00
AlvaroMbeiaDaniel1234 <alvarombeiadanielmiguel@gmail.com>
2025-05-07 21:35:39 +0100

    feat: adicionei imagens reais da actividade

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -2,18 +2,29 @@
 import React from 'react';
 import './styles/gallery.css'; // Arquivo CSS para estilização
 import { motion } from 'framer-motion';
-import img1 from '../images/gallery/lateral.jpg'
-import img2 from '../images/gallery/person1.jpg'
-import img3 from '../images/gallery/person2.jpg'
-import img4 from '../images/gallery/person3.jpg'
+import img1 from '../images/gallery/img1.jpg'
+import img2 from '../images/gallery/img2.jpg'
+import img3 from '../images/gallery/img3.jpg'
+import img4 from '../images/gallery/img4.jpg'
+import img5 from '../images/gallery/img5.jpg'
+import img6 from '../images/gallery/img6.jpg'
+import img7 from '../images/gallery/img7.jpg'
+import img8 from '../images/gallery/img8.jpg'
+
 
 const images = [
-
   img1,
   img2,
   img3,
   img4,  
+  img5, 
+  img6,
+  img7,
+  img8,    
 ];
+
+
+
 
 const Galeria = () => {
   return (
